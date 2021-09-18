fe7f42e7ec798708fa04bb674d43e7b7c115deba
Carter Currin <ccurrin@kochava.com>
2021-09-17 18:05:17 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/23caterpie/datadog-export-logs
+
+go 1.16
+
+require github.com/DataDog/datadog-api-client-go v1.4.0
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	datadog "github.com/DataDog/datadog-api-client-go/api/v2/datadog"
+)
+
+func main() {
+	ctx := datadog.NewDefaultContext(context.Background())
+	configuration := datadog.NewConfiguration()
+	apiClient := datadog.NewAPIClient(configuration)
+
+	var (
+		next *string
+		i    int
+	)
+	for {
+		fmt.Printf("starting page %d\n", i)
+		resp, r, err := apiClient.LogsApi.ListLogsGet(ctx, getParams(next))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `LogsApi.ListLogsGet`: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+			return
+		}
+
+		f := getExportFile(i)
+		err = json.NewEncoder(f).Encode(resp)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error writing to file`: %v\n", err)
+			return
+		}
+		err = f.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error closing file`: %v\n", err)
+			return
+		}
+		next = resp.Meta.Page.After
+		if next == nil {
+			break
+		}
+		fmt.Printf("finshed page %d\n", i)
+		i++
+	}
+}
+
+func getParams(next *string) datadog.ListLogsGetOptionalParameters {
+	filterQuery := `service:aaas-ingestion @msg:"error publishing protobuf payload; dropping"` // string | Search query following logs syntax. (optional)
+	filterFrom := time.Date(2021, time.September, 14, 0, 0, 0, 0, time.UTC)                    // time.Time | Minimum timestamp for requested logs. (optional)
+	filterTo := time.Date(2021, time.September, 17, 0, 0, 0, 0, time.UTC)                      // time.Time | Maximum timestamp for requested logs. (optional)
+	pageLimit := int32(1000)                                                                   // int32 | Maximum number of logs in the response. (optional) (default to 10)
+	optionalParams := datadog.ListLogsGetOptionalParameters{
+		FilterQuery: &filterQuery,
+		FilterFrom:  &filterFrom,
+		FilterTo:    &filterTo,
+		PageCursor:  next,
+		PageLimit:   &pageLimit,
+	}
+
+	return optionalParams
+}
+
+func getExportFile(i int) io.WriteCloser {
+	fileName := fmt.Sprintf("export-%d.json", i)
+	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalln("failed to open export file", err)
+	}
+	return file
+}
